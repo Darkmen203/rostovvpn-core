5a1e4a91ec13156341f81eab8877b20cb7b217e8
Darkmen203 <dima-pivovarov03@mail.ru>
2025-01-18 17:32:37 +0300

    rename and fix go.mod

--- extension/repository/extension_list.go
+++ extension/repository/extension_list.go
@@ -1,6 +1,6 @@
 package repository
 
 import (
-	_ "github.com/hiddify/hiddify-app-demo-extension/hiddify_extension"
-	_ "github.com/hiddify/hiddify-ip-scanner-extension/hiddify_extension"
+	_ "github.com/Darkmen203/rostovvpn-app-demo-extension/hiddify_extension"
+	_ "github.com/Darkmen203/rostovvpn-ip-scanner-extension/hiddify_extension"
 )
